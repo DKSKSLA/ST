0453bfc08e2ddb988c320634b18c4b4316265998
문중현 <jjjung0316@gmail.com>
2023-04-24 03:38:08 +0900

    마커 정보 창 구현 권한요청 오류 해결, 구현완료

--- app/src/main/java/com/example/myapplication/MapActivity.java
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.graphics.PointF;
+import android.icu.text.IDNA;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,24 +13,30 @@ import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    Context context;
+
     private MapView mapView; // 지도 뷰 (fragment)
     private NaverMap naverMap; // 인터페이스 사용을 위한 NaverMap
+    private InfoWindow infoWindow = new InfoWindow();
 
     private final int LOCATION_PERMISSTION_REQUEST_CODE = 1000;
     private UiSettings uiSettings; // 네이버 지도 유저 인터페이스
     private FusedLocationSource locationSource; // 위치를 반환하는 구현체
 
-    private Marker marker;
+    private Marker marker = new Marker();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
+
+        context = this;
 
         // 지도 뷰 객체
         mapView = findViewById(R.id.map_view);
@@ -39,6 +47,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         locationSource = new FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE);
     }
 
+    // 위치 권한 관련 처리
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,  @NonNull int[] grantResults) {
         if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
@@ -51,7 +60,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onMapReady(NaverMap map) {
+    public void onMapReady(NaverMap map) { // NaverMap 객체 생성 시 호출
         naverMap = map;
 
         naverMap.setMinZoom(10.5); // 지도 축소 제한
@@ -62,14 +71,24 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         uiSettings.setLocationButtonEnabled(true); // 현재 위치 버튼 활성화
         naverMap.setLocationSource(locationSource); // TODO 현재 위치 버튼 구현
 
-        marker = new Marker(); // 마커 생성
-        naverMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
+        naverMap.setOnMapClickListener(new NaverMap.OnMapClickListener() { // 지도 클릭 시 이벤트
             @Override
             public void onMapClick(PointF pointF, LatLng clickedLatLng) {
-                if (marker.getMap() != null)
-                    marker.setMap(null); // 기존 마커 제거
                 marker.setPosition(clickedLatLng);
-                marker.setMap(naverMap);
+                marker.setMap(naverMap); // 마커 생성
+
+                // 마커 정보 창 설정
+                infoWindow.setPosition(clickedLatLng);
+                infoWindow.open(marker); // 마커 위치 기반으로 위치 정보 생성
+            }
+        });
+
+        // TODO 마커 정보 창
+        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(context) {
+            @Override
+            public CharSequence getText(InfoWindow infoWindow) {
+                // TODO Reverse Geocoding 사용해서 좌표를 주소로 변환
+                return marker.getPosition().toString();
             }
         });
     }
