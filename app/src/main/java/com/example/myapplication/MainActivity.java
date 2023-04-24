e03cc0f3a91d65e8788f99cba0b9aa0522ab6666
DKSKSLA <Woomin1024@gmail.com>
2023-04-24 22:08:51 +0900

    FAB버튼 추가 FAB버튼누르면 나올화면 추가 할일들 간단하게 보여줄 리사이클러뷰, 사이즈조정등

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,13 +19,16 @@ import android.widget.Toast;
 // 하지만 에러때문에 github에 push가 안됨
 // 해당 오류 없애려면 'ThreeTen' 백포트를 사용해야함 (java.time. -> org.threeten.bp. 으로 import 해야함)
 // 참고 링크 https://scshim.tistory.com/250
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
 import org.threeten.bp.LocalDate;
 import org.threeten.bp.YearMonth;
 import org.threeten.bp.format.DateTimeFormatter;
 
+
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
 {
@@ -31,7 +37,9 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
     private LocalDate selectedDate;
 
     public Button mapButton;
-
+    public Button aaa;
+    public FloatingActionButton fab;
+    public View L3;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -40,10 +48,45 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
 
         // ThreeTen 백포트 사용
         AndroidThreeTen.init(this);
+        L3=(View) findViewById(R.id.L3);
+        ViewGroup.LayoutParams params =L3.getLayoutParams();
+
+        ItemAdapter adapter;
+        RecyclerView recyclerView;
+
+        ArrayList<DataModel> dataModels = new ArrayList();
+
+        dataModels.add(new DataModel("대학","2시","모바일캡스톤 발표","내용"));
+        dataModels.add(new DataModel("대학","5시","웹프로그래밍시험","내용"));
+
+        recyclerView = findViewById(R.id.listRecyclerView);
+        adapter = new ItemAdapter(this,dataModels);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+
 
         initWidgets();
         selectedDate = LocalDate.now();
         setMonthView();
+        aaa=(Button) findViewById(R.id.aaa);
+        aaa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setWeekView();
+
+               params.height=(int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, getResources().getDisplayMetrics());
+               L3.setLayoutParams(params);
+
+            }
+        });
+        fab=(FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // 임시 맵 버튼 동작
         mapButton = (Button) findViewById(R.id.tempMap_btn);
@@ -72,15 +115,27 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         calendarRecyclerView.setLayoutManager(layoutManager);
         calendarRecyclerView.setAdapter(calendarAdapter);
     }
+    private void setWeekView()
+    {
+        monthYearText.setText(monthYearFromDate(LocalDate.now()));
+        ArrayList<String> daysInMonth = WeekArray(LocalDate.now());
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
 
     private ArrayList<String> daysInMonthArray(LocalDate date)
     {
+
         ArrayList<String> daysInMonthArray = new ArrayList<>();
         YearMonth yearMonth = YearMonth.from(date);
 
         int daysInMonth = yearMonth.lengthOfMonth();
 
         LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+
         int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
 
         for(int i = 1; i <= 42; i++)
@@ -96,6 +151,50 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         }
         return  daysInMonthArray;
     }
+
+
+    private ArrayList<String> WeekArray(LocalDate date)
+    {
+
+        ArrayList<String> daysInMonthArray = new ArrayList<>();//배열
+
+        int today=selectedDate.getDayOfMonth();//현재날짜
+
+        YearMonth yearMonth = YearMonth.from(date);//년도와달을 받기
+        int daysInMonth = yearMonth.lengthOfMonth();//년도와달로 해당달최대일수
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+
+        int dayOfWeekValue = selectedDate.getDayOfWeek().getValue();
+
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+        int k=0;
+
+        for(int i=1; i<=dayOfWeekValue;i++)
+        {
+            if(dayOfWeekValue==7){break;}
+            int day=today-dayOfWeekValue;
+
+            if(day<0)//저번달로넘어가면 저번달 마지막날부터
+            {LocalDate selectedDate2 = selectedDate.minusMonths(1);
+                YearMonth yearMonth2 = YearMonth.from(selectedDate2);//년도와달을 받기
+                int daysInMonth2 = yearMonth2.lengthOfMonth();//년도와달로 해당달최대일수
+                day=daysInMonth2+day;}
+
+            daysInMonthArray.add(String.valueOf(day));
+            k++;
+        }
+        for(int i=0;(i+k)<7;i++){
+            int day=today+i;
+            if(day>daysInMonth){day=day-daysInMonth+1;}//다음달로넘어가면 1일부터
+            daysInMonthArray.add(String.valueOf(day));
+        }
+
+
+        return  daysInMonthArray;
+    }
+
+
 
     private String monthYearFromDate(LocalDate date)
     {
