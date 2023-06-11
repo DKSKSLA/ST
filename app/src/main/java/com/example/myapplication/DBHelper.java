a61ecab440e7b233fbdc2110506bd309f9399acc
DKSKSLA <Woomin1024@gmail.com>
2023-06-11 21:29:39 +0900

    UI변경, DB추가,

--- app/src/main/java/com/example/myapplication/DBHelper.java
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -11,11 +11,15 @@ public class DBHelper extends SQLiteOpenHelper {// 데이터 베이스 설정
 
     //데이터베이스 생성자
     public static final String SQL_CREATE_TABLE=
-            "CREATE TABLE "+TableInfo.TABLE_NAME+" ("+
-            TableInfo.COLUMN_NAME_ID+" INTEGER PRIMARY KEY,"+
-            TableInfo.COLUMN_NAME_TIME+" TEXT,"+
+            "CREATE TABLE IF NOT EXISTS "+ TableInfo.TABLE_NAME+" ("+
+            TableInfo.COLUMN_NAME_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            TableInfo.COLUMN_NAME_TIME+" DATETIME,"+
+            TableInfo.COLUMN_NAME_MEMO+" TEXT,"+
+            TableInfo.COLUMN_NAME_TITLE+" TEXT,"+
             TableInfo.COLUMN_NAME_SPACE+" TEXT)";
 
+    public static final String SQL_DELETE_TABLE=
+            "DROP TABLE IF EXISTS " + TableInfo.TABLE_NAME;
 
     public DBHelper(Context context) {
         super(context, "groupDB", null, 1);
@@ -24,13 +28,14 @@ public class DBHelper extends SQLiteOpenHelper {// 데이터 베이스 설정
     @Override
     public void onCreate(SQLiteDatabase db) {
         //생성문 실행
-        db.execSQL("CREATE TABLE groupTBL ( gName CHAR(20) PRIMARY KEY,gNumber INTEGER);");
+        db.execSQL(SQL_CREATE_TABLE);
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //드랍
-        db.execSQL("DROP TABLE IF EXISTS groupTBL");
+        db.execSQL(SQL_DELETE_TABLE);
         onCreate(db);
 
     }
